4d48a9b8b8329743f181d9426d70ee78e7b9e97c
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2021-09-10 23:56:40 +0300

    Added validation for longitude and latitude

--- server/src/models/log.ts
+++ server/src/models/log.ts
@@ -17,10 +17,14 @@ const schema = new mongoose.Schema<Log>(
     latitude: {
       type: Number,
       required: true,
+      min: -90,
+      max: 90,
     },
     longitude: {
       type: Number,
       required: true,
+      min: -180,
+      max: 180,
     },
     visitDate: {
       type: Date,
