cb01669c12f7f8e780655ec58058fb370a6b06ce
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2021-09-10 22:06:00 +0300

    Using notFound and error middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 
+import notFound from './middleware/notFound';
+import error from './middleware/error';
+
 const app = express();
 
 app.use(helmet());
 app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.use(notFound);
+app.use(error);
 
 const port = process.env.PORT || 5000;
 
